0d6058a6f820c5c08385738a8a9542a3d2e9b71c
amenolabode <olabodeamen4@gmail.com>
2023-08-14 00:22:48 +0000

    image compression

--- src/utils/member_data.js
+++ src/utils/member_data.js
@@ -1,49 +1,49 @@
 export const members = [
   {
     imgSrc:
-      "https://res.cloudinary.com/dqicwto8t/image/upload/v1691934780/Kinfolq/members/Marcella_kjnccr.png",
+      "https://res.cloudinary.com/dqicwto8t/image/upload/w_400,q_auto/v1691934780/Kinfolq/members/Marcella_kjnccr.png",
     name: "Marvella Imosimi",
     role: "SWE @ Ebay",
   },
   {
     imgSrc:
-      "https://res.cloudinary.com/dqicwto8t/image/upload/v1691934781/Kinfolq/members/Ojxu_wqxgos.png",
+      "https://res.cloudinary.com/dqicwto8t/image/upload/w_400,q_auto/v1691934781/Kinfolq/members/Ojxu_wqxgos.png",
     name: "Ojxu Junaid",
     role: "UX @ Google",
   },
   {
     imgSrc:
-      "https://res.cloudinary.com/dqicwto8t/image/upload/v1691934780/Kinfolq/members/Malcolm_cikbyr.png",
+      "https://res.cloudinary.com/dqicwto8t/image/upload/w_400,q_auto/v1691934780/Kinfolq/members/Malcolm_cikbyr.png",
     name: "Malcolm Akinie",
     role: "SWE II @ Stack",
   },
   {
     imgSrc:
-      "https://res.cloudinary.com/dqicwto8t/image/upload/v1691934783/Kinfolq/members/Taylor_nhbsao.png",
+      "https://res.cloudinary.com/dqicwto8t/image/upload/w_400,q_auto/v1691934783/Kinfolq/members/Taylor_nhbsao.png",
     name: "Taylor Facen",
     role: "Product @ AngelList",
   },
   {
     imgSrc:
-      "https://res.cloudinary.com/dqicwto8t/image/upload/v1691934780/Kinfolq/members/Aminat_c5pmdb.png",
+      "https://res.cloudinary.com/dqicwto8t/image/upload/w_400,q_auto/v1691934780/Kinfolq/members/Aminat_c5pmdb.png",
     name: "Aminat Iriafen",
     role: "Research @ Qunatilope",
   },
   {
     imgSrc:
-      "https://res.cloudinary.com/dqicwto8t/image/upload/v1691934780/Kinfolq/members/Eni_qljoqz.png",
+      "https://res.cloudinary.com/dqicwto8t/image/upload/w_400,q_auto/v1691934780/Kinfolq/members/Eni_qljoqz.png",
     name: "Eni Asebiomo",
     role: "Design @ Percapita",
   },
   {
     imgSrc:
-      "https://res.cloudinary.com/dqicwto8t/image/upload/v1691934783/Kinfolq/members/Rahama_fodyc6.png",
+      "https://res.cloudinary.com/dqicwto8t/image/upload/w_400,q_auto/v1691934783/Kinfolq/members/Rahama_fodyc6.png",
     name: "Rahama Muhammad",
     role: "SWE @ Mastercard",
   },
   {
     imgSrc:
-      "https://res.cloudinary.com/dqicwto8t/image/upload/v1691934780/Kinfolq/members/Jonathan_est0yo.png",
+      "https://res.cloudinary.com/dqicwto8t/image/upload/w_400,q_auto/v1691934780/Kinfolq/members/Jonathan_est0yo.png",
     name: "Jonathan Anderson",
     role: "SWE @ Vise",
   },
